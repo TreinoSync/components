d4e48499656e68ce7c766ca7f68daddef46b4bff
isaacbatista26 <isaacbatista2266@gmail.com>
2024-02-20 21:23:14 -0300

    Feat: Navbar Desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Navigation/Navbar"
 export default function Home() {
   return (
     <h1>
+      <Navbar />
       <Footer />
     </h1>
   );
